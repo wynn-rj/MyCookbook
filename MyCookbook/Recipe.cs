ae0b9cf5e674361c022c2d47047d9c78c420f21e
Robert J Wynn <wynn.134@osu.edu>
2017-02-21 13:03:15 -0500

    Bug fix
    Fixed bug that caused a crash when image file were deleted

--- MyCookbook/Recipe.cs
+++ MyCookbook/Recipe.cs
@@ -179,6 +179,7 @@ namespace MyCookbook
                     MessageBox.Show(message, caption, buttons);
 
                     imageLocation = "";
+                    imageLocation = recipePictureBox.ImageLocation;
                 }
             }
             else
